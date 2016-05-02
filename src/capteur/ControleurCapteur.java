ea79282a6b4447b19bc7173e10badcb6c22096b7
Dyden Ung <lioehurn2.0@hotmail.com>
2016-05-02 16:28:39 -0400

    vrai commit controleur capteur gestion employe

---
+++ src/capteur/ControleurCapteur.java
@@ -0,0 +1,29 @@
+package capteur;
+
+import java.util.HashMap;
+
+public class ControleurCapteur {
+	
+	private HashMap<String, Capteur> listeCapteur = new HashMap<String, Capteur>();
+	
+	public ControleurCapteur() {
+		this.genererListeCapteurs();
+	}
+	
+	private void genererListeCapteurs() {
+		int code = 123;
+		for(int i = 0; i < 3; i++) {
+			code++;
+			Capteur capteur = new Capteur("C"+code);
+			System.out.println("C"+code);
+			listeCapteur.put(capteur.getIdChaudiere(), capteur);
+		}
+	}
+	
+	public Capteur getCapteur(String idChaudiere) {
+		Capteur capteur = null;
+		capteur = listeCapteur.get(idChaudiere);
+		
+		return capteur;
+	}
+}
---
+++ src/employe/GestionEmploye.java
@@ -0,0 +1,38 @@
+package employe;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GestionEmploye {
+	
+	private ArrayList<String> listeAbsent = new ArrayList<String> ();
+	private HashMap<String, Employe> listeEmploye = new HashMap<String, Employe>();
+	
+	public GestionEmploye() {
+		this.genererAbsence();
+		this.genererListeEmploye();
+	}
+	
+	private void genererAbsence() {
+		Employe employe1 = new Employe("em20161111", "Bob", "Langlois", "A!111aaa");
+		Employe employe2 = new Employe("em20162222", "Tony", "Bobo", "B!222bbb");
+		listeAbsent.add(employe1.getPrenom() + " " + employe1.getNom());
+		listeAbsent.add(employe2.getPrenom() + " " + employe2.getNom());
+	}
+	
+	private void genererListeEmploye() {
+		ResponsableChaudiere resp = new ResponsableChaudiere("re20161111", "John", "Leclair", "R#111aaa");
+		Superviseur sup = new Superviseur("su20162222", "Hugo", "Lajoie", "S%222bbb");
+		listeEmploye.put(resp.getId(), resp);
+		listeEmploye.put(sup.getId(), sup);
+	}
+
+	public ArrayList<String> getListeAbsent() {
+		return listeAbsent;
+	}
+
+	public HashMap<String, Employe> getListeEmploye() {
+		return listeEmploye;
+	}
+	
+}
